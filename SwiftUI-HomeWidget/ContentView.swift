bc2c7d970c85c52fcff8b1ab0a67a75037907eec
Jaydip <jaydip.appdeveloper@gmail.com>
2021-04-27 10:33:28 +0530

    Initial Commit

---
+++ SwiftUI-HomeWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-HomeWidget
+//
+//  Created by Jaydip  on 27/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-HomeWidget/SwiftUI_HomeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HomeWidgetApp.swift
+//  SwiftUI-HomeWidget
+//
+//  Created by Jaydip  on 27/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HomeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
